f6a0f07b4f03b497436ae087d673ad5e5f6def1e
raflirach <rafli060395@gmail.com>
2021-03-16 08:19:49 +0700

    add: login & register route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  post '/register', to: 'users#register'
+  post '/login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
