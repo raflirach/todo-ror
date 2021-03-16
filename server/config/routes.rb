2cbba8d44d5dbed71ecc5eb8c516e4d3054af69b
raflirach <rafli060395@gmail.com>
2021-03-16 13:03:02 +0700

    add: routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :todos
+  patch '/todos/:id/status', to: 'todos#updateStatus'
+  patch '/todos/:id/priority', to: 'todos#updatePriority'
   post '/register', to: 'users#register'
   post '/login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
