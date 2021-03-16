137cb7c1c581ec115054139750cc2f956e7e5d55
raflirach <rafli060395@gmail.com>
2021-03-16 13:02:13 +0700

    add: todo model

---
+++ server/app/models/todo.rb
@@ -0,0 +1,7 @@
+class Todo < ApplicationRecord
+  belongs_to :user
+
+  validates :status, inclusion: [true, false]
+  validates :priority, inclusion: [true, false]
+  validates :title, length: { minimum: 2 }
+end
