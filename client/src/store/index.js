faba640f150d9547e93024aaa010127e420bad03
raflirach <rafli060395@gmail.com>
2021-03-17 12:20:26 +0700

    add: action add & delete todo

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -7,8 +7,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: [],
+    isAdd: false
   },
   mutations: {
+    insertTodo (state, payload) {
+      state.todos = payload
+    },
+    changeIsAdd (state, payload) {
+      state.isAdd = payload
+    }
   },
   actions: {
     async register (_, payload) {
@@ -40,6 +48,56 @@ export default new Vuex.Store({
         })
         localStorage.access_token = data['Access-Token']
         router.push('/')
+      } catch (err) {
+        console.log(err.response.data)
+      }
+    },
+    async fetchTodo ({ commit }) {
+      try {
+        const { data } = await axios({
+          method: 'GET',
+          url: '/todos',
+          headers: {
+            'Access-Token': localStorage.access_token
+          }
+        })
+        commit('insertTodo', data)
+      } catch (err) {
+        console.log(err.response.data)
+      }
+    },
+    async addTodo ({ commit }, payload) {
+      const { title, dueDate, status, priority } = payload
+      try {
+        await axios({
+          method: 'POST',
+          url: '/todos',
+          headers: {
+            'Access-Token': localStorage.access_token
+          },
+          data: {
+            title,
+            due_date: dueDate,
+            status,
+            priority
+          }
+        })
+        commit('changeIsAdd', false)
+        this.dispatch('fetchTodo')
+      } catch (err) {
+        console.log(err.response.data)
+      }
+    },
+    async deleteTodo (_, id) {
+      try {
+        await axios({
+          method: 'DELETE',
+          url: `/todos/${id}`,
+          headers: {
+            'Access-Token': localStorage.access_token
+          }
+        })
+        this.dispatch('fetchTodo')
       } catch (err) {
         console.log(err.response.data)
       }
