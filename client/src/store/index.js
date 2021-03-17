30cf1903ead457be8e3eddfcb53428efbf4f83f3
raflirach <rafli060395@gmail.com>
2021-03-17 08:30:53 +0700

    add: login & register actions

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '../api/axios'
+import router from '../router'
 
 Vue.use(Vuex)
 
@@ -9,7 +11,38 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
-  },
-  modules: {
+    async register (_, payload) {
+      try {
+        const { email, password } = payload
+        await axios({
+          method: 'POST',
+          url: '/register',
+          data: {
+            email,
+            password
+          }
+        })
+        router.push('/login')
+      } catch (err) {
+        console.log(err.response.data)
+      }
+    },
+    async login (_, payload) {
+      try {
+        const { email, password } = payload
+        const { data } = await axios({
+          method: 'POST',
+          url: '/login',
+          data: {
+            email,
+            password
+          }
+        })
+        localStorage.access_token = data['Access-Token']
+        router.push('/')
+      } catch (err) {
+        console.log(err.response.data)
+      }
+    }
   }
 })
