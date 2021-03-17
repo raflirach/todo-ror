f33d94a5606a1a2e1667b8576f92035caa258810
raflirach <rafli060395@gmail.com>
2021-03-17 12:19:47 +0700

    add: todoCard

---
+++ client/src/components/TodoCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="content relative border rounded w-full shadow-md text-left p-2 mb-2">
+    <div>{{ todo.title }}</div>
+    <div>{{ todo.due_date }}</div>
+    <div class="option absolute top-0 right-2 flex gap-1">
+      <button><i class="fas fa-pen"></i></button>
+      <button @click="deleteTodo"><i class="fas fa-trash"></i></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoCard',
+  props: ['todo'],
+  methods: {
+    deleteTodo () {
+      const { id } = this.todo
+      this.$store.dispatch('deleteTodo', id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .option {
+    display: none;
+  }
+  .content:hover .option {
+    display: flex;
+  }
+</style>
