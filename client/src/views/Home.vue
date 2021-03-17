2d612b80d5d77d4a7f417268e1ee4a8706c8f250
raflirach <rafli060395@gmail.com>
2021-03-17 12:18:54 +0700

    add: modal

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="w-full h-screen flex flex-col pt-14">
     <navbar />
-    <div> Home </div>
+    <todo-container :todos="todos"/>
+    <modal v-if="isAdd"/>
   </div>
 </template>
 
 <script>
+import Modal from '../components/Modal.vue'
 import Navbar from '../components/Navbar.vue'
+import TodoContainer from '../components/TodoContainer.vue'
 // @ is an alias to /src
 
 export default {
-  components: { Navbar },
-  name: 'Home'
+  components: { Navbar, TodoContainer, Modal },
+  name: 'Home',
+  methods: {
+    fetchTodo () {
+      this.$store.dispatch('fetchTodo')
+    }
+  },
+  computed: {
+    todos () {
+      return this.$store.state.todos
+    },
+    isAdd () {
+      return this.$store.state.isAdd
+    }
+  },
+  created () {
+    this.fetchTodo()
+  }
 }
 </script>
