fc77c8979c68775217eed6de965089575c396092
raflirach <rafli060395@gmail.com>
2021-03-17 08:42:07 +0700

    add: navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-12 w-full absolute top-0 shadow-md flex justify-between px-4 items-center">
+    <div>Todo</div>
+    <a class="bg-blue-400 py-1 px-4 rounded shadow-md text-white font-bold cursor-pointer" @click="logout">Logout</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+    logout () {
+      localStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    Home
+  <div class="w-full h-screen flex flex-col pt-14">
+    <navbar />
+    <div> Home </div>
   </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue'
 // @ is an alias to /src
 
 export default {
+  components: { Navbar },
   name: 'Home'
 }
 </script>
