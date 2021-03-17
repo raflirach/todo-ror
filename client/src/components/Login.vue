153837aa2a5fdbb645f74fec614921379568f5f4
raflirach <rafli060395@gmail.com>
2021-03-17 08:30:23 +0700

    add: method login

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-1/3 h-1/2 shadow-lg border rounded flex flex-col justify-center items-center">
     <div class="text-2xl font-bold mb-4">Login</div>
-    <form>
-      <input type="text" class="border border-blue-500 w-2/3 rounded p-2 mb-2 text-sm" placeholder="email" />
-      <input type="password" class="border border-blue-500 w-2/3 rounded p-2 mb-2 text-sm" placeholder="password" />
+    <form @submit.prevent="login">
+      <input type="text" v-model="email" class="border border-blue-500 w-2/3 rounded p-2 mb-2 text-sm" placeholder="email" />
+      <input type="password" v-model="password" class="border border-blue-500 w-2/3 rounded p-2 mb-2 text-sm" placeholder="password" />
       <div>
         <button class="bg-blue-500 rounded py-1 px-4 text-white shadow-md mt-4">Login</button>
         <div class="text-sm mt-4">Don't have an account? <a class="text-blue-500 cursor-pointer" @click="navigate">Register</a></div>
@@ -15,9 +15,20 @@
 <script>
 export default {
   name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
   methods: {
     navigate () {
       this.$router.push('/register')
+    },
+    login () {
+      const { email, password } = this
+      const payload = { email, password }
+      this.$store.dispatch('login', payload)
     }
   }
 }
