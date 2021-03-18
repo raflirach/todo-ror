6139f75e52185984e523b338ff0345596a8b9003
raflirach <rafli060395@gmail.com>
2021-03-18 08:16:30 +0700

    add router guard

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -34,4 +34,10 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.path !== '/login' && !localStorage.access_token) next({ name: 'Login' })
+  else if (to.path === '/login' && localStorage.access_token) next({ name: 'Home' })
+  else next()
+})
+
 export default router
